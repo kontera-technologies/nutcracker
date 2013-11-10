757e5b624815f1c0dd335923d4d167030e64e26e
eranb <levieb2@gmail.com>
2013-11-10 17:19:25 +0200

    fixing tests

--- lib/nutcracker.rb
+++ lib/nutcracker.rb
@@ -48,6 +48,7 @@ module Nutcracker
     def start *args
       return self if attached? or running?
       @pid = ::Process.spawn Nutcracker.executable, *command
+      Process.detach(@pid)
       puts "IN HERE"
       sleep 1
       abort "Nutcracker failed to start" unless running?
